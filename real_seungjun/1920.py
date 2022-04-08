903b69297181242fef52a0f0463fa107fae1e23e
신승준 <alohajune22@gmail.com>
2022-04-08 14:20:55 +0900

    refactoring

---
+++ real_seungjun/1920.py
@@ -0,0 +1,31 @@
+import sys
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+
+m = int(input())
+b = list(map(int, input().split()))
+
+def check(x):
+    lt = 0
+    rt = n - 1
+
+    while lt <= rt:
+        mid = (lt + rt) // 2
+
+        if a[mid] == x:
+            return 1
+
+        elif a[mid] > x:
+            rt = mid - 1
+
+        else:
+            lt = mid + 1
+
+    return 0
+
+for i in b:
+    print(check(i))
