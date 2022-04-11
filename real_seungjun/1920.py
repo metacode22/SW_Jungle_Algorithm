a96c14aba04328c6d7147b0718da4caf11926ee4
신승준 <jun@sinseungjun-ui-MacBookAir.local>
2022-04-11 22:17:37 +0900

    1629_multiply

--- real_seungjun/1920.py
+++ real_seungjun/1920.py
@@ -1,5 +1,5 @@
 import sys
-# sys.stdin = open('input.txt')
+sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 n = int(input())
@@ -12,6 +12,7 @@ b = list(map(int, input().split()))
 def check(x):
     lt = 0
     rt = n - 1
+    print(rt)
 
     while lt <= rt:
         mid = (lt + rt) // 2
