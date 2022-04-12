7ff94cb00588a5fe7f2d93210174985cf5830a14
신승준 <jun@sinseungjun-ui-MacBookAir.local>
2022-04-12 23:21:30 +0900

    refactoring

--- real_seungjun/prac.py
+++ real_seungjun/prac.py
@@ -1,4 +1,14 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
+n = int(input())
+
+queue = deque(range(1, n + 1))
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+    
+print(queue[0])
