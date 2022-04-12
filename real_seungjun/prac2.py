7ee51be35216669ba8368e07aa18f697e6dd6f1e
신승준 <jun@sinseungjun-ui-MacBookAir.local>
2022-04-12 11:28:53 +0900

    test

--- real_seungjun/prac2.py
+++ real_seungjun/prac2.py
@@ -1,23 +1,13 @@
 import sys
+import heapq
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
 a = []
-for _ in range(n):
-    a.append(list(map(int, input().split())))
-    
-M, k = map(int, input().split())
-b = []
-for _ in range(M):
-    b.append(list(map(int, input().split())))
-    
-c = [[0] * k for _ in range(n)]
 
-for i in range(n):
-    for j in range(k):
-        for l in range(M):
-            c[i][j] += a[i][l] * b[l][j]
-            
-for i in range(len(c)):
-    print(*c[i])
+heapq.heappush(a, 1)
+heapq.heappush(a, 5)
+heapq.heappush(a, 3)
+heapq.heappush(a, 0)
+
+print(a)
